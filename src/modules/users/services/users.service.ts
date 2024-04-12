8bb325ed381844534d0cf64fb2daee0f408bc40a
Ianara-cs <ianarasilva333@gmail.com>
2024-04-12 16:12:45 -0300

    refactor: criptografia da senha e tratamento de erro ao criar usuário

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,7 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common'
+import { BadRequestException, Inject, Injectable } from '@nestjs/common'
 import { IUserRepository } from '../repositories/interfaces/IUsersRepository'
 import { CreateUserDto } from '../dtos/create-user.dto'
 import { User } from '../entities/user.entity'
+import { hash } from 'bcrypt'
 
 @Injectable()
 export class UsersService {
@@ -10,8 +11,28 @@ export class UsersService {
     private usersRepository: IUserRepository,
   ) {}
 
-  async createUser(createUser: CreateUserDto): Promise<User> {
-    const newUser = await this.usersRepository.create(createUser)
+  async createUser({
+    cpf,
+    email,
+    name,
+    password,
+  }: CreateUserDto): Promise<User> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (user) {
+      throw new BadRequestException('User already exists!')
+    }
+
+    const saltOrRounds = 10
+    const passwordHash = await hash(password, saltOrRounds)
+
+    const newUser = await this.usersRepository.create({
+      cpf,
+      email,
+      name,
+      password: passwordHash,
+    })
+
     return newUser
   }
 }
