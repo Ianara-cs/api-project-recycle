8494a04da24ccee9c21bc0aac03f913f3d4e0234
Ianara-cs <ianarasilva333@gmail.com>
2024-05-25 14:22:44 -0300

    feat: adicionando o swagger

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post } from '@nestjs/common'
 import { UsersService } from '../services/users.service'
 import { User } from '../entities/user.entity'
 import { CreateUserDto } from '../dtos/create-user.dto'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
